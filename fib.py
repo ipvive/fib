2dfe662c10ae60f55c65a4f31bd37e5f83073562
Nathaniel Thurston <njt@ipvive.com>
2024-06-12 10:11:24 -0700

    oops

--- fib.py
+++ fib.py
@@ -3,7 +3,7 @@ def fib(n):
     return 1
   return fib(n-1) + fib(n-2)
 
-if __name__ == "++main__":
-  n = input("enter a number: ")
+if __name__ == "__main__":
+  n = int(input("enter a number: "))
   f = fib(n)
   print(f"fib({n}) = {f}")
